e04eb38f4d5adf2d540625c81df5eba386575dbb
coza_nostra7 <begzodmuminovb@gmail.com>
2026-03-07 21:59:38 +0100

    add dependencies for user in dependencies.py

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from starlette import status
 
 from src.models import EmployeeRole
+from src.services.auth import AuthService
 from src.utils.db_manager import DBManager
 from starlette.requests import Request
 from src.database import async_session_maker
@@ -22,7 +23,18 @@ class Pagination_params(BaseModel):
 PaginationDep = Annotated[Pagination_params,Depends()]
 
 
+def get_token(request: Request) -> str:
+    token = request.cookies.get('access_token', None)
+    if not token:
+        raise HTTPException(status_code=401,detail='You are not authorized')
+    return token
 
+def get_current_user_id(token: str = Depends(get_token)) -> int:
+    data = AuthService().decode_token(token)
+    return data['user_id']
+
+
+UserIDDep = Annotated[int,Depends(get_current_user_id)]
 
 
 def get_db_manager():
